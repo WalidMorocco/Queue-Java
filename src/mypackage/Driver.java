944e6ae82aa4264af5396c1a16d453ea703d7d80
Walid Amar <walidamarpro@gmail.com>
2022-09-12 17:04:05 -0400

    Final Commit

--- src/mypackage/Driver.java
+++ src/mypackage/Driver.java
@@ -3,8 +3,19 @@ package mypackage;
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Create Queue
+		Queue queue = new Queue();
+		
+		// Add Integers to the Queue
+		queue.enqueue(new Integer(7));
+		queue.enqueue(new Integer(10));
+		queue.enqueue(new Integer(-3));
+		
+		// Remove first element of the Queue
+		queue.dequeue();
+		
+		// Print the first element of the queue
+		System.out.println(queue.firstEl());
 	}
 
 }
--- src/mypackage/Queue.java
+++ src/mypackage/Queue.java
@@ -1,5 +1,36 @@
 package mypackage;
 
 public class Queue {
-
+	private java.util.LinkedList<Object> queue = new java.util.LinkedList<Object>();
+	
+	// Constructor
+	public Queue() {
+		
+	}
+	
+	// Clear the Queue
+	public void clear() {
+		queue.clear();
+	}
+	
+	// Return if the Queue is empty or not
+	public boolean isEmpty() {
+		return queue.isEmpty();
+	}
+	
+	// Return the Object at the start of the Queue (First Object)
+	public Object firstEl() {
+		return queue.getFirst();
+	}
+	
+	// Remove the Object at the start of the Queue
+	public Object dequeue() {
+		return queue.removeFirst();
+	}
+	
+	// Add an Object to the Queue
+	public void enqueue(Object el) {
+		queue.addLast(el);
+	}
+	
 }
